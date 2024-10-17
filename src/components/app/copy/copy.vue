c0af5b90c036702eacb5b83bae27135498bf1041
Barbara <barbara.jozefowska@top-flight-digital.com>
2024-10-17 08:56:31 +0200

    feat: introduce copy occurrences

---
+++ src/components/app/copy/copy.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+    type: {
+        type: String,
+        required: true,
+    },
+    inline: {
+        type: Boolean,
+        default: false,
+    },
+})
+</script>
+
+<!-- TODO: Remove hardcoded [style] -->
+<template>
+    <div class="app-copy" style="color:#fff" />
+</template>
