0f569847ba4fc4958acd701c0c0ca1f6a20e426a
Barbara <barbara.jozefowska@top-flight-digital.com>
2024-10-21 18:04:48 +0200

    feat: add tabs occurrences

---
+++ src/components/app/tabs/tabs.stories.ts
@@ -0,0 +1,80 @@
+import AppTabs from './tabs.vue';
+import PageA from './stories/page-a.vue';
+import PageB from './stories/page-b.vue';
+import PageC from './stories/page-c.vue';
+import { ref } from 'vue';
+
+export default {
+    component: AppTabs,
+    render: props => ({
+        components: { AppTabs, PageA, PageB, PageC },
+        setup() {
+            const tab = ref(props.items[0].slug);
+
+            return {
+                props,
+                tab,
+            };
+        },
+        template: `<app-tabs v-bind="props" v-model="tab" />`,
+    }),
+    args: {
+        items: [],
+        size: `small`,
+    },
+};
+
+export const MainTabs = {
+    args: {
+        size: `large`,
+        items: [
+            {
+                slug: `product`,
+                label: `Product`,
+                component: PageA,
+            },
+            {
+                slug: `scans`,
+                label: `Scans`,
+                component: PageB,
+            },
+            {
+                slug: `packages`,
+                label: `Packages`,
+                component: PageC,
+            },
+        ],
+    },
+};
+
+export const CurrentHistoryTabs = {
+    args: {
+        size: `small`,
+        items: [
+            {
+                slug: `current`,
+                label: `Current`,
+            },
+            {
+                slug: `history`,
+                label: `History`,
+            },
+        ],
+    },
+};
+
+export const MonthlyAnuallyTabs = {
+    args: {
+        size: `small`,
+        items: [
+            {
+                slug: `monthly`,
+                label: `Monthly`,
+            },
+            {
+                slug: `anually`,
+                label: `Anually`,
+            },
+        ],
+    },
+};
---
+++ src/components/app/tabs/tabs.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps({
+    items: {
+        type: Array,
+        required: true,
+    },
+    size: {
+        type: String,
+        required: true,
+    },
+});
+
+const model = defineModel();
+</script>
+
+<template>
+    <div class="app-tabs" />
+</template>
