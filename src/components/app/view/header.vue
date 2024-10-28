0f578df7ffdb5de5c4b8fb61a39a10b5217fa836
Przemek <przemek.gosek@top-flight-digital.com>
2024-10-28 11:02:47 +0100

    feat: add view header component

---
+++ src/components/app/view/header.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+defineProps({
+    headline: {
+        type: String,
+    },
+    subline: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <div class="view__header">
+        <app-copy
+            v-if="headline"
+            type="Title/h1"
+            color="light"
+            v-text="headline"
+        />
+
+        <app-copy
+            v-if="subline"
+            type="Title/h4"
+            color="mid"
+            v-text="subline"
+        />
+    </div>
+</template>
