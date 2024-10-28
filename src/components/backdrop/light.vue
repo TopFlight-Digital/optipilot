ffc4c28cfbfd5579a1d4e6592978f0405a97eb16
Przemek <przemek.gosek@top-flight-digital.com>
2024-10-28 11:02:23 +0100

    feat: add backdrop component

---
+++ src/components/backdrop/light.vue
@@ -0,0 +1,369 @@
+<template>
+    <svg
+        width="600"
+        height="900"
+        viewBox="0 0 600 900"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="backdrop-light"
+    >
+        <g clip-path="url(#clip0_154_18682)">
+            <g clip-path="url(#clip1_154_18682)">
+                <circle
+                    cx="501"
+                    cy="311"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="449"
+                    cy="423"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="231"
+                    cy="348"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="536"
+                    cy="635"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="259.805"
+                    cy="2.80516"
+                    r="2"
+                    transform="rotate(37.6463 259.805 2.80516)"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="299"
+                    cy="619"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="597.805"
+                    cy="140.805"
+                    r="2"
+                    transform="rotate(37.6463 597.805 140.805)"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="272"
+                    cy="792"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="135"
+                    cy="748"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="532"
+                    cy="813"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="367"
+                    cy="498.5"
+                    rx="2"
+                    ry="1.5"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="309"
+                    cy="584"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="427"
+                    cy="611.5"
+                    rx="2"
+                    ry="1.5"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="80"
+                    cy="82"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="305.002"
+                    cy="554.484"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="433.002"
+                    cy="11.4844"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="140.002"
+                    cy="367.484"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="251"
+                    cy="82"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="476.002"
+                    cy="554.484"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="311.002"
+                    cy="367.484"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="119.231"
+                    cy="188.377"
+                    r="2"
+                    transform="rotate(37.6463 119.231 188.377)"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="1.93239"
+                    cy="1.72897"
+                    rx="1.93239"
+                    ry="1.72897"
+                    transform="matrix(-0.920919 0.389754 -0.527915 -0.849297 572.385 254.084)"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="8.8171"
+                    cy="451.29"
+                    r="2"
+                    transform="rotate(37.6463 8.8171 451.29)"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="441"
+                    cy="86"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="501.002"
+                    cy="370.984"
+                    rx="2"
+                    ry="1.5"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="139.227"
+                    cy="850.592"
+                    r="2"
+                    transform="rotate(37.6463 139.227 850.592)"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="267.227"
+                    cy="307.592"
+                    r="2"
+                    transform="rotate(37.6463 267.227 307.592)"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="1.87717"
+                    cy="1.78877"
+                    rx="1.87717"
+                    ry="1.78877"
+                    transform="matrix(0.843604 0.536965 -0.682911 0.730502 101.865 66.5117)"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="505"
+                    cy="194"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="565.002"
+                    cy="459.984"
+                    rx="2"
+                    ry="1.5"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="251.938"
+                    cy="432.198"
+                    r="2"
+                    transform="rotate(37.6463 251.938 432.198)"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="1.87717"
+                    cy="1.78877"
+                    rx="1.87717"
+                    ry="1.78877"
+                    transform="matrix(0.843604 0.536965 -0.682911 0.730502 86.5762 169.33)"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="303"
+                    cy="194"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="528.002"
+                    cy="666.484"
+                    r="2"
+                    fill="#1D242D"
+                />
+
+                <ellipse
+                    cx="363.002"
+                    cy="459.984"
+                    rx="2"
+                    ry="1.5"
+                    fill="#1D242D"
+                />
+
+                <circle
+                    cx="91.9968"
+                    cy="308.819"
+                    r="2"
+                    transform="rotate(37.6463 91.9968 308.819)"
+                    fill="#1D242D"
+                />
+
+                <g filter="url(#filter0_f_154_18682)">
+                    <path
+                        d="M10 254.161C10 183.38 65.5167 126 134 126C202.483 126 258 183.38 258 254.161C258 324.943 202.483 382.322 134 382.322C65.5167 382.322 10 324.943 10 254.161Z"
+                        fill="#5F63A0"
+                    />
+                </g>
+            </g>
+
+            <circle
+                cx="356"
+                cy="76.4844"
+                r="2"
+                fill="#1D242D"
+            />
+
+            <circle
+                cx="100"
+                cy="324.484"
+                r="2"
+                fill="#1D242D"
+            />
+
+            <circle
+                cx="169"
+                cy="537.484"
+                r="2"
+                fill="#1D242D"
+            />
+        </g>
+
+        <defs>
+            <filter
+                id="filter0_f_154_18682"
+                x="-208.5"
+                y="-92.5"
+                width="685"
+                height="693.322"
+                filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB"
+            >
+                <feFlood
+                    flood-opacity="0"
+                    result="BackgroundImageFix"
+                />
+
+                <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="BackgroundImageFix"
+                    result="shape"
+                />
+
+                <feGaussianBlur
+                    stdDeviation="109.25"
+                    result="effect1_foregroundBlur_154_18682"
+                />
+            </filter>
+
+            <clipPath id="clip0_154_18682">
+                <rect
+                    width="2097"
+                    height="2606"
+                    fill="white"
+                    transform="translate(-735 -709)"
+                />
+            </clipPath>
+
+            <clipPath id="clip1_154_18682">
+                <rect
+                    width="1590"
+                    height="1824"
+                    fill="white"
+                    transform="translate(-735 -709)"
+                />
+            </clipPath>
+        </defs>
+    </svg>
+</template>
+
+<style lang="scss" scoped>
+.backdrop-light {
+    position: absolute;
+    top: 0;
+    left: 0px;
+    pointer-events: none;
+}
+</style>
