d22d574a0c4994d33338c9b459696ba0af7d64fa
Filip Matynia <koncepcjonista37@gmail.com>
2024-10-28 18:29:06 +0100

    feat: Introduce progress-bar component

---
+++ src/components/app/progress-bar/progress-bar.stories.ts
@@ -0,0 +1,12 @@
+import ProgressBar from './progress-bar.vue';
+
+export default {
+    component: ProgressBar,
+};
+
+export const progressBar = {
+    args: {
+        numerator: 1,
+        denominator: 3,
+    },
+};
---
+++ src/components/app/progress-bar/progress-bar.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    numerator: {
+        type: Number,
+        reguired: true,
+    },
+    denominator: {
+        type: Number,
+        default: () => 100,
+    },
+});
+
+const progressBarStyle = computed(() => ({
+    '--progress-bar-width': `${(props.numerator as number / props.denominator) * 100}%`,
+}));
+</script>
+
+<template>
+    <div class="app-progress-bar">
+        <div
+            class="app-progress-bar__indicator"
+            :style="progressBarStyle"
+        />
+    </div>
+</template>
+
+<style lang="scss">
+.app-progress-bar {
+    border: 1px solid var(--color-edge-primary);
+    background: var(--color-backdrop-primary);
+    border-radius: 24px;
+
+    &__indicator {
+        background: var(--color-primary);
+        width: var(--progress-bar-width);
+        height: 10px;
+        border-radius: 20px;
+    }
+}
+</style>
