89b37482f28534830d794c09fb944ca3efdd122f
Barbara <barbara.jozefowska@top-flight-digital.com>
2024-11-02 16:27:19 +0100

    feat: introduce choice occurrences

---
+++ src/components/app/choice/choice.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { FIELD_PROPS } from '../field/field';
+
+const props = defineProps({
+    ...FIELD_PROPS,
+});
+
+// TODO: replace `string` with type based on props.items
+const model = defineModel<string>();
+
+</script>
+
+<template>
+    <app-field
+        :label="label"
+        :hint="hint"
+        :required="required"
+        class="app-choice"
+    >
+        <template #label:after>
+            <slot name="label:after" />
+        </template>
+    </app-field>
+</template>
+
+<style lang="scss">
+</style>
