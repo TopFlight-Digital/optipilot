70065f5fa5a209e4c2d6de9b5861f6391dc27918
Barbara <barbara.jozefowska@top-flight-digital.com>
2024-11-02 20:06:13 +0100

    feat: introduce hypothesis component occurrences

---
+++ src/components/bloc/hypothesis/hypothesis.stories.ts
@@ -0,0 +1,29 @@
+import BlocHypothesis from './hypothesis.vue';
+
+export default {
+    component: BlocHypothesis,
+};
+
+export const Hypothesis1 = {
+    args: {
+        index: 1,
+        title: `Optimise product listings`,
+        subtitle: `Reducing decision fatigue and improving navigation can encourage users to progress and ultimately convert.`,
+    },
+};
+
+export const Hypothesis2 = {
+    args: {
+        index: 2,
+        title: `Enhance product imagery`,
+        subtitle: `Lorem ipsum dolor sit amet consectetur. Sagittis sed vitae integer eleifend dictum malesuada morbi.dictum malesuada morbi.`,
+    },
+};
+
+export const Hypothesis3 = {
+    args: {
+        index: 3,
+        title: `Preview customer reviews`,
+        subtitle: `Lorem ipsum dolor sit amet consectetur. Sagittis sed vitae integer eleifend dictum malesuada morbi.dictum malesuada morbi.`,
+    },
+};
---
+++ src/components/bloc/hypothesis/hypothesis.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+    /**
+     * The ordinal number of the hypothesis, starting from 1.
+     */
+    index: {
+        type: Number,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="bloc-hypothesis" />
+</template>
