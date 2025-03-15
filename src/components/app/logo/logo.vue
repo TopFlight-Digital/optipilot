b68c814660d22202e0ffb5679ec7280646c33f3a
Filip <koncepcjonista37@gmail.com>
2025-03-15 12:23:36 +0100

    feat: Add topflight logo

--- src/components/app/logo/logo.vue
+++ src/components/app/logo/logo.vue
@@ -4,27 +4,37 @@ import logo from '@/icons/logo.svg';
 
 <template>
     <div class="logo">
-        <app-icon :name=logo />
+        <div class="logo__optipilot">
+            <app-icon :name=logo />
 
-        <app-copy
-            type="Body/b1-reg"
-            color="f6"
-            v-text="`OptiPilot`"
-        />
+            <app-copy
+                class="logo__optipilot-label"
+                type="Body/b1-reg"
+                color="f6"
+                v-text="`OptiPilot`"
+            />
+        </div>
     </div>
 </template>
 
 <style lang="scss">
 .logo {
     display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: .5rem;
-    height: 24px;
+    flex-direction: column;
+    gap: 2px;
+    margin-top: 3px;
 
-    svg {
-        width: 20px;
-        height: 20.43px;
+    &__optipilot {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: .5rem;
+        height: 24px;
+
+        svg {
+            width: 20px;
+            height: 20px;
+        }
     }
 }
 </style>
