0ffd1b0e290f10c13397800319391ba457917926
Barbara <barbara.jozefowska@top-flight-digital.com>
2025-04-30 07:31:33 +0200

    fix: change imports

--- backend/prompt/constants.ts
+++ backend/prompt/constants.ts
@@ -1,4 +1,4 @@
-import { ChatModel } from "openai/resources/chat/index.mjs";
+import type { ChatModel } from "openai/resources/chat/index.mjs";
 
 export const DEFAULT_MODEL = `gpt-4o` as const satisfies ChatModel;
 
