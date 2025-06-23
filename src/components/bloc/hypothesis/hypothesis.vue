82f1b08bcf5f21eb76e2e492b946118e6b33e740
Macbook <fashinaoluwaseun36@gmail.com>
2025-06-23 15:16:13 +0100

    refactored svg to component

--- src/components/bloc/hypothesis/hypothesis.vue
+++ src/components/bloc/hypothesis/hypothesis.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import AppHeart from "@/components/app/icon/heart.vue";
+
 const props = defineProps({
     /**
      * The ordinal number of the hypothesis, starting from 1.
@@ -26,7 +28,9 @@ const props = defineProps({
     },
 });
 
-const emit = defineEmits([`toggle-like`]);
+const emit = defineEmits<{
+    (event: `toggle-like`): void;
+}>();
 
 const { hostFavicon, scanId } = useBloc();
 const scan = useScanById(scanId);
@@ -43,35 +47,7 @@ const iconSource = computed(
             :aria-label="props.liked ? 'Unlike' : 'Like'"
             @click.stop="emit('toggle-like')"
         >
-            <svg
-                v-if="props.liked"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="white"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                />
-            </svg>
-
-            <svg
-                v-else
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                />
-            </svg>
+            <app-heart :liked="props.liked" />
         </button>
         <!-- End Heart Icon -->
         <div
